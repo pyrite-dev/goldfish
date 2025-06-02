90b43b389353e6f96a869a5ed199ab0464cc2cb4
NishiOwO <nishi@nishi.boats>
2025-06-02 20:03:48 +0900

    add example

---
+++ examples/audio.c
@@ -0,0 +1,46 @@
+#include <gf_core.h>
+#include <gf_audio.h>
+
+#include <stdio.h>
+
+int run = 1;
+gf_engine_t* engine;
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <signal.h>
+
+void handler(int sig){
+	run = 0;
+}
+#endif
+
+int main(int argc, char** argv){
+	if(argc != 2){
+		fprintf(stderr, "Usage: %s path\n", argv[0]);
+		return 1;
+	}
+	gf_engine_begin();
+	engine = gf_engine_create_ex("unused", 1, "data", argv, argc);
+	if(engine != NULL){
+		gf_audio_t* audio = gf_audio_create(engine);
+		gf_audio_id_t id = gf_audio_load_file(audio, argv[1]);
+
+		if(id != -1){
+			gf_audio_resume(audio, id);
+			while(run && !gf_audio_is_over(audio, id)){
+#ifdef _WIN32
+				Sleep(10);
+#else
+				usleep(10 * 1000);
+#endif
+			}
+		}
+
+		gf_audio_destroy(audio);
+		gf_engine_destroy(engine);
+	}
+	gf_engine_end();
+}
---
+++ examples/premake5.lua
@@ -0,0 +1,60 @@
+flags("MultiProcessorCompile")
+
+workspace("Examples")
+	configurations({
+		"Debug",
+		"Release"
+	})
+	platforms({
+		"Native",
+		"Win32",
+		"Win64"
+	})
+	defaultplatform("Native")
+
+filter("platforms:Win32")
+	system("windows")
+	architecture("x86")
+	gccprefix("i686-w64-mingw32-")
+
+filter("platforms:Win64")
+	system("windows")
+	architecture("x86_64")
+	gccprefix("x86_64-w64-mingw32-")
+
+filter({})
+
+include ".."
+
+function call_me(name)
+	project(name)
+		flags("RelativeLinks")
+		kind("ConsoleApp")
+		language("C")
+		targetdir("bin/%{cfg.buildcfg}/%{cfg.platform}")
+		objdir("obj/%{cfg.buildcfg}/%{cfg.platform}")
+		targetname(name)
+		includedirs({
+			"../include"
+		})
+		files({
+			name .. ".c"
+		})
+		links({
+			"GoldFish"
+		})
+		-- Call this if you are gonna use my engine...
+		gf_link_stuffs("options:engine=static")
+		filter("configurations:Debug")
+			defines({
+				"DEBUG"
+			})
+			symbols("On")
+		filter("configurations:Release")
+			defines({
+				"NDEBUG"
+			})
+			optimize("On")
+end
+
+call_me("audio")
