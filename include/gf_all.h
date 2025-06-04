961bd69bc4d47d13628a25f2c03ffd0f69046b37
NishiOwO <nishi@nishi.boats>
2025-06-04 13:55:56 +0900

    add gf_all.h

---
+++ include/gf_all.h
@@ -0,0 +1,38 @@
+/**
+ * @file gf_all.h
+ * @~english
+ * @brief Include all GoldFish headers
+ */
+
+#include <gf_assert.h>
+#include <gf_audio.h>
+#include <gf_checksum.h>
+#include <gf_client.h>
+#include <gf_command.h>
+#include <gf_core.h>
+#include <gf_draw.h>
+#include <gf_draw_driver.h>
+#include <gf_draw_platform.h>
+#include <gf_file.h>
+#include <gf_font.h>
+#include <gf_graphic.h>
+#include <gf_gui.h>
+#include <gf_gui_component.h>
+#include <gf_image.h>
+#include <gf_input.h>
+#include <gf_log.h>
+#include <gf_lua.h>
+#include <gf_math.h>
+#include <gf_mesh.h>
+#include <gf_model.h>
+#include <gf_network.h>
+#include <gf_physics.h>
+#include <gf_prop.h>
+#include <gf_resource.h>
+#include <gf_server.h>
+#include <gf_sound.h>
+#include <gf_texture.h>
+#include <gf_thread.h>
+#include <gf_unicode.h>
+#include <gf_util.h>
+#include <gf_version.h>
---
+++ tool/genall.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+cat > include/gf_all.h << EOF
+/**
+ * @file gf_all.h
+ * @~english
+ * @brief Include all GoldFish headers
+ */
+
+EOF
+cd include
+for i in *.h; do
+	case "$i" in
+		gf_*drv.h)
+			:
+			;;
+		gf_*_static.h)
+			:
+			;;
+		gf_opengl.h)
+			:
+			;;
+		gf_all.h)
+			:
+			;;
+		gf_config.h)
+			:
+			;;
+		gf_pre.h)
+			:
+			;;
+		gf_macro.h)
+			:
+			;;
+		*)
+			echo "#include <$i>" >> gf_all.h
+			;;
+	esac
+done
