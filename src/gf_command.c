a65c783be232f085cde4ebcc020bd4519035776f
NishiOwO <nishi@nishi.boats>
2025-06-15 22:03:34 +0900

    fix

--- src/gf_command.c
+++ src/gf_command.c
@@ -70,6 +70,7 @@ void gf_command_file(gf_engine_t* engine, const char* path) {
 const char* gf_command_join_args(const char** args, int start, int end) {
 	int len = 0;
 	int i;
+	int x;
 	char* new;
 	/* First "1 + " for '\0', remaining for ' ' */
 	for(i = start; i <= end; i++) len += 1 + strlen(args[i]);
